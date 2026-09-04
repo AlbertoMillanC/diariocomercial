1a6d018b899e3855ce4e64cc21ede2b6d98d7fe9
Alberto Millán C <albertomillanc@users.noreply.github.com>
2026-09-03 22:48:16 -0500

    admin, migraciones y deje el idioma en espanol

--- diariocomercial/settings.py
+++ diariocomercial/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -116,3 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'inicio'
+LOGOUT_REDIRECT_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'diariocomercial@localhost'
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    Establecimiento,
+    Perfil,
+    Venta,
+    Compra,
+    Retencion,
+    EnvioReporte,
+    Auditoria,
+)
 
-# Register your models here.
+
+admin.site.register(Establecimiento)
+admin.site.register(Perfil)
+admin.site.register(Venta)
+admin.site.register(Compra)
+admin.site.register(Retencion)
+admin.site.register(EnvioReporte)
+admin.site.register(Auditoria)
